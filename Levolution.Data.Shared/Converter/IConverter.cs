2c636ca0e74ee7fb7bd8e03f52a8ae900099ea1d
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-13 05:06:40 +0900

    Add: IConverter, ProbableExtensions

---
+++ Levolution.Data.Shared/Converter/IConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Levolution.Data.Shared.Converter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TInput"></typeparam>
+    /// <typeparam name="TOutput"></typeparam>
+    public interface IConverter<TInput, TOutput>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        TOutput Convert(TInput value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        TInput ConvertBack(TOutput value);
+    }
+}
---
+++ Levolution.Data.Shared/Probable/ProbableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Levolution.Data
+{
+    public static class ProbableExtensions
+    {
+        private static Random DefaultRandom { get; } = new Random();
+
+        public static IProbable Choose(this IEnumerable<IProbable> items)
+            => items.Choose(DefaultRandom);
+
+        public static IProbable Choose(this IEnumerable<IProbable> items, Random random)
+        {
+            if (!items.Any())
+            {
+                throw new ArgumentException("The items parameter is empty.", nameof(items));
+            }
+
+            var array = items.OrderByDescending(x => x.Probability).ToArray();
+            var sum = array.Sum(x => x.Probability);
+            var p = random.NextDouble() * sum;
+
+            foreach(var item in array)
+            {
+                if (p <= item.Probability) { return item; }
+                p -= item.Probability;
+            }
+            return array[array.Length - 1];
+        }
+    }
+}
