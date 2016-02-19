4bc1651e1128014525a1e4a71e2d9227e9831ada
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-20 03:13:11 +0900

    Add: IRange interface.

---
+++ Levolution.Data.Shared/IRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Levolution.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRange<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        T Minimun { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        T Maximum { get; }
+    }
+}
